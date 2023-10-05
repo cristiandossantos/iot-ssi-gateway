53b726d0b87cf2b452eebdede144e3409049a408
Cristian Alves dos Santos <cristiann.alves@gmail.com>
2023-10-05 00:58:58 -0300

    First commit

---
+++ src/main/java/com/ufsc/lrg/iot/ssi/gateway/IotSsiGateway.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.ufsc.lrg.iot.ssi.gateway;
+
+/**
+ *
+ * @author 07429377980
+ */
+public class IotSsiGateway {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
